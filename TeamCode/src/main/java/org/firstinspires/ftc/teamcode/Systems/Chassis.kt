c42876797bb5b46ca3465e12d50b7f5bd6645401
Frida <a01198437@itesm.mx>
2019-10-24 17:25:05 -0500

    Converted mecanum data to data classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Chassis.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Chassis.kt
@@ -2,31 +2,16 @@ package org.firstinspires.ftc.teamcode.Systems
 
 import org.firstinspires.ftc.teamcode.core.Controller
 
-class MecanumMotorState {
-    public var topLeftAngularSpeed = 0.0
-    public var topRightAngularSpeed = 0.0
-    public var downLeftAngularSpeed = 0.0
-    public var downRightAngularSpeed = 0.0
+data class MecanumMotorState(var topLeftAngularSpeed: Double = 0.0, var topRightAngularSpeed: Double = 0.0, var downLeftAngularSpeed: Double = 0.0, var downRightAngularSpeed: Double = 0.0)
 
-}
+data class MecanumMotorValues(var topLeftSpeed: Double = 0.0, var topRightSpeed: Double = 0.0, var downLeftSpeed: Double = 0.0, var downRightSpeed: Double = 0.0)
 
-class MecanumMotorValues {
-    public var topLeftSpeed = 0.0
-    public var topRightSpeed = 0.0
-    public var downLeftSpeed = 0.0
-    public var downRightSpeed = 0.0
-}
+data class Twist2D(var vx: Double = 0., var vy: Double = 0.0, var w: Double = 0.0)
 
-class Twist2D {
-    public var vx = 0.0
-    public var vy = 0.0
-    public var w = 0.0
-
-}
 
 class MecanumKinematics(var xDistanceFromWheelToCenter: Double, var yDistanceFromWheelToCenter: Double, var wheelRadius: Double) {
 
-    public fun calcInverseKinematics(vx: Double, vy: Double, w: Double): MecanumMotorValues {
+    fun calcInverseKinematics(vx: Double, vy: Double, w: Double): MecanumMotorValues {
         val values = MecanumMotorValues()
         values.topLeftSpeed = vx - vy - xDistanceFromWheelToCenter * w - yDistanceFromWheelToCenter * w
         values.topRightSpeed = vx + vy + xDistanceFromWheelToCenter * w + yDistanceFromWheelToCenter * w
@@ -36,7 +21,7 @@ class MecanumKinematics(var xDistanceFromWheelToCenter: Double, var yDistanceFro
 
     }
 
-    public fun calcForwardKinematics(state: MecanumMotorState): Twist2D {
+    fun calcForwardKinematics(state: MecanumMotorState): Twist2D {
         val result = Twist2D()
 
         result.vx = (state.topLeftAngularSpeed + state.topRightAngularSpeed + state.downLeftAngularSpeed + state.downRightAngularSpeed) * wheelRadius / 4.0
@@ -52,12 +37,14 @@ class MecanumKinematics(var xDistanceFromWheelToCenter: Double, var yDistanceFro
  */
 
 class Chassis : Controller() {
-    public fun move(vx: Double, vy: Double, w: Double){
-        val values= kinematics.calcInverseKinematics(vx, vy, w)
+    fun move(vx: Double, vy: Double, w: Double) {
+        val values = kinematics.calcInverseKinematics(vx, vy, w)
         writeMotors(values)
     }
-    fun writeMotors(values:MecanumMotorValues){
+
+    fun writeMotors(values: MecanumMotorValues) {
 
     }
+
     var kinematics = MecanumKinematics(0.5, 0.5, 1.0)
 }
