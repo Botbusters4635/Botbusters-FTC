74776f9a5219de34b025c220939c9474751a9599
Hiram <hirammunozr@outlook.com>
2019-12-17 15:50:46 -0600

    all working autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpModes/OnlyGrabBlue.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpModes/OnlyGrabBlue.kt
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.LinearOpModes
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.firstinspires.ftc.teamcode.controllers.Intake
 import org.firstinspires.ftc.teamcode.controllers.chassis.MecanumMoveCommand
 import org.firstinspires.ftc.teamcode.controllers.chassis.PositionChassis
@@ -9,7 +11,7 @@ import org.firstinspires.ftc.teamcode.controllers.arm.SynchronizedArm
 import org.firstinspires.ftc.teamcode.core.Coordinate
 import org.firstinspires.ftc.teamcode.core.EctoLinearOpMode
 
-@Autonomous(name = "OnlyGrabRed", group = "Red")
+@Autonomous(name = "OnlyGrabBlue", group = "Blue")
 class OnlyGrabBlue : EctoLinearOpMode() {
 
     val chassis = PositionChassis()
@@ -27,18 +29,27 @@ class OnlyGrabBlue : EctoLinearOpMode() {
     }
 
     override fun runOpMode() {
-        chassis.runToPositionBlocking(Coordinate(0.275, 0.0))
-        chassis.turnToAngleBlocking(45.0)
-        intake.power = 1.0
-        chassis.moveTimed(MecanumMoveCommand(vx = 0.3, theta = 45.0), 2.2)
-        intake.power = 0.0
-        chassis.runToPositionBlocking(Coordinate(0.1, 0.0))
-        chassis.runToPositionBlocking(Coordinate(0.1, -0.2))
-        chassis.turnToAngleBlocking(-90.0)
+        chassis.runToPositionBlocking(Coordinate(0.5, 0.15))
+        chassis.turnToAngleBlocking(-40.0)
         intake.power = -1.0
-        chassis.maxAutoVx = 0.5
-        chassis.maxAutoVy = 0.5
-        chassis.runToPositionBlocking(Coordinate(0.2, -0.1))
+        chassis.moveTimed(MecanumMoveCommand(vx = 0.15, theta = -40.0), 1.8)
+        runBlocking {
+            delay(1000)
+        }
+        intake.power = 0.0
+        chassis.runToPositionBlocking(Coordinate(0.1, 0.2))
+        chassis.maxAutoVx = 0.7
+        chassis.maxAutoVy = 0.7
+        chassis.turnToAngleBlocking(90.0)
+        runBlocking { delay(500) }
+        intake.power = 1.0
+        chassis.moveTimed(MecanumMoveCommand(vx = 0.7, theta = 90.0), 1.0)
+        chassis.moveTimed(MecanumMoveCommand(vx = -0.7, theta = 90.0), 0.8)
+
+
+        chassis.runToPositionBlocking(Coordinate(0.7, 0.35))
+
+        runBlocking { delay(10000) }
     }
 
 }
